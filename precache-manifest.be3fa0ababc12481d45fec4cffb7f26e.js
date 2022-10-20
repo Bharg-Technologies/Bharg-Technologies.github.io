6e8f54b2f342a08a49a5b49465c521ae8677d6b0
prasanthbattula <prasanthbattula5@gmail.com>
2022-10-20 08:41:24 +0530

    gitpages deploy

---
+++ precache-manifest.be3fa0ababc12481d45fec4cffb7f26e.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c28b726169236fc92e5",
+    "url": "/static/js/0.de7c67cf.chunk.js"
+  },
+  {
+    "revision": "6e6f167b2deeb575ea21",
+    "url": "/static/js/1.72098039.chunk.js"
+  },
+  {
+    "revision": "d5b8dbb74ef54b8b782f",
+    "url": "/static/css/main.a941258e.chunk.css"
+  },
+  {
+    "revision": "d5b8dbb74ef54b8b782f",
+    "url": "/static/js/main.189c8f5b.chunk.js"
+  },
+  {
+    "revision": "f45011eec85db2619471",
+    "url": "/static/js/runtime~main.7565c19a.js"
+  },
+  {
+    "revision": "9c1a6adb9f24591f0044",
+    "url": "/static/js/4.b99edd8d.chunk.js"
+  },
+  {
+    "revision": "00a02a7e501de946c5ca",
+    "url": "/static/js/5.941b6f69.chunk.js"
+  },
+  {
+    "revision": "e586dc2ed69467b23adb",
+    "url": "/static/js/6.d853e72a.chunk.js"
+  },
+  {
+    "revision": "8f2027f730d5fbb749a5",
+    "url": "/static/js/7.b56e8c53.chunk.js"
+  },
+  {
+    "revision": "abd8311bc9b208f614a3",
+    "url": "/static/js/8.7e369244.chunk.js"
+  },
+  {
+    "revision": "b198d9a86d95c68527d9",
+    "url": "/static/js/9.fcbe8ad6.chunk.js"
+  },
+  {
+    "revision": "0ad1a86f92760c6df012",
+    "url": "/static/js/10.4d6eac95.chunk.js"
+  },
+  {
+    "revision": "3920b8ff871f8aeea292",
+    "url": "/static/js/11.fe3c150c.chunk.js"
+  },
+  {
+    "revision": "116de889feb9ecce54c3",
+    "url": "/static/js/12.262b208b.chunk.js"
+  },
+  {
+    "revision": "6fcdf2a085cd9774f909",
+    "url": "/static/js/13.cf781bd0.chunk.js"
+  },
+  {
+    "revision": "f5d503fa93177754853cc5b5b479f8f7",
+    "url": "/static/media/pro-bg.f5d503fa.png"
+  },
+  {
+    "revision": "13607a5c3a60aed54d52e36897c80d90",
+    "url": "/static/media/pic1.13607a5c.png"
+  },
+  {
+    "revision": "bbea39e8fb99c9b7ff15e78c4e10d304",
+    "url": "/static/media/p1-bditto.bbea39e8.jpeg"
+  },
+  {
+    "revision": "bd58c772088eb69795ac090f3cddd267",
+    "url": "/static/media/thoughtz.bd58c772.jpg"
+  },
+  {
+    "revision": "45af469d9cbcfc893bb4ac6afa42d483",
+    "url": "/static/media/p3-meethome.45af469d.jpeg"
+  },
+  {
+    "revision": "2b488c1d8372e5c9cbac6085010be54b",
+    "url": "/static/media/p2-shnipp.2b488c1d.jpeg"
+  },
+  {
+    "revision": "fb9a1beae65b7a96e58deef91507d106",
+    "url": "/static/media/p4-brillwork.fb9a1bea.jpeg"
+  },
+  {
+    "revision": "069ca608b2da73a746d35bcd5562f65a",
+    "url": "/static/media/our-services1.069ca608.png"
+  },
+  {
+    "revision": "c54f9315da174587dac57000ea3dbf60",
+    "url": "/static/media/our-services2.c54f9315.png"
+  },
+  {
+    "revision": "e6cd8027f57ebd733b2935354f4d3f71",
+    "url": "/static/media/our-services3.e6cd8027.png"
+  },
+  {
+    "revision": "2b80d98676b765cb255d4169bbd92a30",
+    "url": "/static/media/cross.2b80d986.svg"
+  },
+  {
+    "revision": "4e9dff460e336673856861dd88fbb9af",
+    "url": "/index.html"
+  }
+];
